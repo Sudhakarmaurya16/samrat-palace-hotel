8627d802a23e7fda07bfb4ba7843836c39868c8b
Sudhakar Maurya <sudhakarmaurya71208@gmail.com>
2026-01-30 14:40:27 +0530

    Fixed import to lowercase home.css

--- samrat-palace-frontend/src/pages/Home.jsx
+++ samrat-palace-frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"; // Navigation ke liye
-import "../styles/Home.css";
+import "../styles/home.css";
 import { fetchAllRooms } from "../services/api";
 
 function Home() {
